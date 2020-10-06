fc772b2ad62e6d4b510798084c1d91cc59f1ae3b
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-06 09:27:49 +0300

    ..

--- Analogy.LogViewer.EventSource.Clef/EventSourceProviderFactory.cs
+++ Analogy.LogViewer.EventSource.Clef/EventSourceProviderFactory.cs
@@ -7,6 +7,9 @@ namespace Analogy.LogViewer.EventSource.Clef
 {
     public class EventSourceProviderFactory : IAnalogyDataProvidersFactory
     {
+        //serilog id to combine it:
+        //public Guid FactoryId { get; set; } = new Guid("513A4393-425E-4054-92D4-6A816983E51F");
+
         public Guid FactoryId { get; set; } = EventSourceFactory.Id;
         public string Title { get; set; } = "Clef EventSource Provider";
 
